3992ef3beb92abe173e8d1448bb076806ab83645
Pathem <nexus556.45@gmail.com>
2018-07-28 14:38:09 +0900

    SplitTeam Command Reinforcement
    - Move players to assigned team's VC channel command
    - Recall players from channels after match command
    thus, the command became conversational

--- DiscordNetPlus/Commands/SplitTeamCommand.cs
+++ DiscordNetPlus/Commands/SplitTeamCommand.cs
@@ -3,27 +3,97 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord.Commands;
+using Discord.WebSocket;
 using DiscordNetPlus.Extentions;
+using DiscordNetPlus.Interaction;
 
 namespace DiscordNetPlus.Commands
 {
+    [Group("split"), Alias("st"), RequireContext(ContextType.Guild)]
     public class SplitTeamCommand : GuildCommandBase
     {
-        [Command("split"), Alias("st"), RequireContext(ContextType.Guild)]
+        [Command("")]
         public async Task SplitTeam(int teamNumber = 2)
         {
-            var channelmembers = ConnectedVoiceChannel().Users;
-            var result = Split(channelmembers, teamNumber);
+            var members = ConnectedVoiceChannel().Users;
+            var resultTeams = Split(members, teamNumber);
 
             var embed = EmbedBuilderFactory();
 
             var counter = 1;
-            foreach (var team in result)
+            foreach (var team in resultTeams)
             {
-                embed.AddField($"Team #{counter++}", string.Join(", ", team.Select(user => user.Nickname)));
+                embed.AddField(
+                    $"Team #{counter} ({GetVoiceChannelReletive(counter - 1)?.Name ?? ""})",
+                    string.Join(Environment.NewLine, team.Select(user => user.Nickname ?? user.Username)) + Environment.NewLine
+                    );
+                counter++;
             }
 
-            await ReplyEmbed(embed, $"Splitted {channelmembers.Count} members into {teamNumber} groups.");
+            await ReplyEmbed(embed, $"`{members.Count}` 人のプレイヤーを `{teamNumber}` つのチームに分けました");
+
+            Context.Client.RelationMemory.Memorise(
+               Context.Channel,
+               new SplitTeamConversation
+               {
+                   Teams = resultTeams,
+                   TopChannel = GetVoiceChannelReletive(0)
+               }
+               );
+        }
+
+        [Command("assign"), Alias("a")]
+        public async Task Assign()
+        {
+            var conv = Context.Client.RelationMemory.Recall(Context.Channel, new SplitTeamConversation().Topic);
+            var stConv = (SplitTeamConversation)conv;
+
+            var counter = 0;
+            foreach (var team in stConv.Teams)
+            {
+                counter++;
+
+                if (counter == 1)
+                    continue;
+
+                var destChannel = GetVoiceChannelReletive(counter - 1);
+                if (destChannel == null)
+                    break;
+
+                foreach (var user in team)
+                {
+                    await user.ModifyAsync(u =>
+                    {
+                        u.Channel = destChannel;
+                    });
+                }
+            }
+
+            await ReplyAsync("```チームごとのチャンネルにプレイヤーを移動しました```");
+        }
+
+        [Command("recall"), Alias("r")]
+        public async Task Recall()
+        {
+            var conv = Context.Client.RelationMemory.Recall(Context.Channel, new SplitTeamConversation().Topic);
+            var stConv = (SplitTeamConversation)conv;
+
+            for (var team = 1; team < stConv.Teams.Count(); team++)
+            {
+                var channel = GetVoiceChannelReletive(team);
+                if (channel == null)
+                    break;
+
+                foreach (var user in channel.Users)
+                {
+                    await user.ModifyAsync(u =>
+                    {
+                        u.Channel = stConv.TopChannel;
+                    });
+                }
+            }
+
+            await ReplyAsync("```全プレイヤーを元のチャンネルに戻しました```");
         }
 
         private static IEnumerable<IEnumerable<T>> Split<T>(IEnumerable<T> sourceList, int teamNumber)
@@ -33,11 +103,27 @@ namespace DiscordNetPlus.Commands
             var rnd = new Random(); //Declaring this outside as a field will make this method thread-unsafe.
             var shuffledList = list.OrderBy(member => rnd.Next()).ToList(); //note: result will duplicate when called at the same time. 
 
-            var memberPerTeam = list.Count / teamNumber;
+            var memberPerTeam = (int)Math.Ceiling((double)list.Count / teamNumber); //because int / operator floors decimals.
 
             var result = shuffledList.SplitList(memberPerTeam);
 
             return result;
+        }
+
+        private SocketVoiceChannel GetVoiceChannelReletive(int offset)
+        {
+            if (offset == 0)
+                return ConnectedVoiceChannel();
+
+            var voicechannels = Context.Guild.VoiceChannels;
+
+            return voicechannels.FirstOrDefault(c => c.Position == ConnectedVoiceChannel().Position + offset);
+        }
+        private class SplitTeamConversation : IConversation
+        {
+            public object Topic { get; set; } = nameof(SplitTeamCommand);
+            public IEnumerable<IEnumerable<SocketGuildUser>> Teams { get; set; }
+            public SocketVoiceChannel TopChannel { get; set; }
         }
     }
 }
